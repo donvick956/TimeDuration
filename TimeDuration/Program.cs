5f92243a408b0b2f3d9d4c1cd0ef02f16d5da7f2
victor okereke <donvick956@gmail.com>
2021-11-10 16:44:29 +0100

    Add project files.

---
+++ TimeDuration/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TimeDuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "c:\\RoyalCrestCollege\\studentsrecord.LogFile.txt";
+            File.Create(path);
+            Stack<DateTime> time = new Stack<DateTime>();
+            time.Push(DateTime.Now);
+            Thread.Sleep(10000);
+
+            time.Push(DateTime.Now);
+            Console.WriteLine(time.Peek());
+            Thread.Sleep(10000);
+
+            time.Push(DateTime.Now);
+            Console.WriteLine(time.Peek());
+            Thread.Sleep(10000);
+
+
+
+            foreach (var second in time)
+            {
+                File.AppendAllLines(path, new String[] { second.ToString() });
+            }
+
+          
+        }
+    }
+}
